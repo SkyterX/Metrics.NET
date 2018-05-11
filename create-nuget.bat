5763b7326641c1db2b8f2758ee46d8cde6708ec4
avk <Andrew.Kostousov@gmail.com>
2018-05-11 19:09:45 +0500

    fix nuget-packing script to produce forked package

--- create-nuget.bat
+++ create-nuget.bat
@@ -1,6 +1,9 @@
+rd /S /Q .\bin\Release
 rd /S /Q .\Publishing\lib
 
-call build.bat
+.nuget\NuGet.exe restore Metrics.sln
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MsBuild.exe" Metrics.sln /target:Clean;Rebuild /p:Configuration=Release /verbosity:m /p:zip=false
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 md .\Publishing\lib
@@ -10,26 +13,5 @@ copy .\bin\Release\Metrics.dll .\Publishing\lib\net45\
 copy .\bin\Release\Metrics.xml .\Publishing\lib\net45\
 copy .\bin\Release\Metrics.pdb .\Publishing\lib\net45\
 
-copy .\bin\Release\Nancy.metrics.dll .\Publishing\lib\net45\
-copy .\bin\Release\Nancy.metrics.xml .\Publishing\lib\net45\
-copy .\bin\Release\Nancy.metrics.pdb .\Publishing\lib\net45\
-
-copy .\bin\Release\owin.metrics.dll .\Publishing\lib\net45\
-copy .\bin\Release\owin.metrics.xml .\Publishing\lib\net45\
-copy .\bin\Release\owin.metrics.pdb .\Publishing\lib\net45\
-
-copy .\bin\Release\Metrics.NLog.dll .\Publishing\lib\net45\
-copy .\bin\Release\Metrics.NLog.xml .\Publishing\lib\net45\
-copy .\bin\Release\Metrics.NLog.pdb .\Publishing\lib\net45\
-
 .\.nuget\NuGet.exe pack .\Publishing\Metrics.Net.nuspec -OutputDirectory .\Publishing
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-.\.nuget\NuGet.exe pack .\Publishing\Nancy.Metrics.nuspec -OutputDirectory .\Publishing
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-.\.nuget\NuGet.exe pack .\Publishing\Owin.Metrics.nuspec -OutputDirectory .\Publishing
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-.\.nuget\NuGet.exe pack .\Publishing\Metrics.NLog.nuspec -OutputDirectory .\Publishing
 if %errorlevel% neq 0 exit /b %errorlevel%
