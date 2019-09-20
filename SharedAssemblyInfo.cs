825816198f591e85853b3aac293c92cf1e3f99f2
iperevoschikov <iperevoschikov@skbkontur.ru>
2019-09-20 10:01:21 +0500

    clean-up sln

--- SharedAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Iulian Margarintescu @ Erata.NET, The Recognos Metrics.NET Team")]
-[assembly: AssemblyProduct("Metrics.NET")]
-[assembly: AssemblyCopyright("Copyright Iulian Margarintescu, The Recognos Metrics.NET Team © 2016")]
-[assembly: AssemblyVersion("0.4.7")]
-[assembly: AssemblyFileVersion("0.4.7")]
-[assembly: AssemblyInformationalVersion("0.4.7")]
